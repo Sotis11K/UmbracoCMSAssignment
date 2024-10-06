33526a634f288bae53268ba8dec935a0fe3987c8
Sotis11K <89807156+Sotis11K@users.noreply.github.com>
2024-10-06 18:56:15 +0200

    done

--- UmbracoCMSAssignment/wwwroot/scripts/main.js
+++ UmbracoCMSAssignment/wwwroot/scripts/main.js
@@ -3,3 +3,21 @@ const currentURL = window.location.href;
 if (currentURL === 'https://localhost:44389/contact' || currentURL === 'https://localhost:44389/contact/') {
     document.querySelector(".section-yourProject").style.backgroundColor = "white"
 } 
+
+
+if (currentURL === "https://localhost:44389/about" || currentURL === 'https://localhost:44389/about/') {
+    document.querySelector(".navigation-links-about").classList.add("active");
+
+}
+else if (currentURL === "https://localhost:44389/service" || currentURL === 'https://localhost:44389/service/' || currentURL === 'https://localhost:44389/riskmanagement' || currentURL === 'https://localhost:44389/riskmanagement/') {
+    document.querySelector(".navigation-links-services").classList.add("active");
+
+}
+else if (currentURL === "https://localhost:44389/our-projects" || currentURL === 'https://localhost:44389/our-projects/' || currentURL === 'https://localhost:44389/annualreport' || currentURL === 'https://localhost:44389/annualreport/')  {
+    document.querySelector(".navigation-links-projects").classList.add("active");
+
+}
+else if (currentURL === "https://localhost:44389/contact" || currentURL === 'https://localhost:44389/contact/') {
+    document.querySelector(".navigation-links-contact").classList.add("active");
+
+}
