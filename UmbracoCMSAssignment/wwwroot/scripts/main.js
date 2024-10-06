4779e23d2b752e4be2100d6be7b209aace5a1eab
Sotis11K <89807156+Sotis11K@users.noreply.github.com>
2024-10-06 04:26:51 +0200

    big push

---
+++ UmbracoCMSAssignment/wwwroot/scripts/main.js
@@ -0,0 +1,5 @@
+const currentURL = window.location.href;
+
+if (currentURL === 'https://localhost:44389/contact' || currentURL === 'https://localhost:44389/contact/') {
+    document.querySelector(".section-yourProject").style.backgroundColor = "white"
+} 
